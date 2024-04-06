b35e321472581a3c5f917ceb08429d7fea8f7569
luckysetiawan <luckysetiawan0802@gmail.com>
2024-04-06 19:12:07 +0700

    refactor a to link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import logo from '../assets/images/logo.png'
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.png";
 
 const Navbar = () => {
   return (
@@ -7,47 +8,40 @@ const Navbar = () => {
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="flex h-20 items-center justify-between">
             <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-            <a 
-              className="flex flex-shrink-0 items-center mr-4" 
-              href="/index.html"
-            >
-              <img
-                  className="h-10 w-auto"
-                  src={ logo }
-                  alt="React Jobs"
-              />
-              <span className="hidden md:block text-white text-2xl font-bold ml-2">
+              <Link className="flex flex-shrink-0 items-center mr-4" to="/">
+                <img className="h-10 w-auto" src={logo} alt="React Jobs" />
+                <span className="hidden md:block text-white text-2xl font-bold ml-2">
                   React Jobs
-              </span>
-            </a>
-            <div className="md:ml-auto">
-              <div className="flex space-x-2">
-                <a
-                  href="/index.html"
-                  className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >
-                  Home
-                </a>
-                <a
-                  href="/jobs.html"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >
-                  Jobs
-                </a>
-                <a
-                  href="/add-job.html"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >
-                  Add Job
-                </a>
+                </span>
+              </Link>
+              <div className="md:ml-auto">
+                <div className="flex space-x-2">
+                  <Link
+                    to="/"
+                    className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    to="/jobs"
+                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  >
+                    Jobs
+                  </Link>
+                  <Link
+                    to="/add-job"
+                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  >
+                    Add Job
+                  </Link>
+                </div>
               </div>
-            </div>
             </div>
           </div>
         </div>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
